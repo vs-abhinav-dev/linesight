573ef7bf756994b0eb109ab823c3fad4f5296844
vs-abhinav-dev <abhinav21221@gmail.com>
2025-11-21 16:21:37 +0530

    commit for report

--- scripts/launch_game_pb.sh
+++ scripts/launch_game_pb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Optional: environment variables to improve performance
-TMLoaderPath="$HOME/.wine/drive_c/users/MrShah_21/AppData/Local/TMLoader"
-
+TMLoaderPath="~/.wine/drive_c/users/[your-linux-account]/AppData/Local/TMLoader"
+# usually .wine/drive_c/users/[your-linux-account]/AppData/Local/TMLoader
 # Basic launch command:
 #exec wine "./TMInterfaceTesting.exe" "/configstring=set custom_port $1" #WINEFSYNC=1 STAGING_SHARED_MEMORY=1 STAGING_WRITECOPY=1
 
